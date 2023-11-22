117e7e4322b9492ba43f4f680d4000cb02152c90
GamzeNurAslan <150529563+GamzeNurAslan@users.noreply.github.com>
2023-11-22 17:50:34 +0300

    Add files via upload
    Karşılaştırma Operatörleri

---
+++ JavaApplication104.java
@@ -0,0 +1,43 @@
+package javaapplication104;
+
+public class JavaApplication104 {
+public static void main(String[] args) {
+     int A = 10;
+    int B = 20;
+    int C = 30;
+    int D = 40;
+    
+    // == Operatörü
+    System.out.println(A == B);
+    System.out.println(A == C);
+    System.out.println(C == D);
+    
+    // != Eşit değil operatörü
+    System.out.println(A != D);
+    System.out.println(A != C);
+    System.out.println(C != B);
+    
+    // > Büyüktür operatörü
+    System.out.println(A > D);
+    System.out.println(D > C);
+    System.out.println(C > B);
+    
+    // >= Büyük-Eşittir operatörü
+    System.out.println(A >= D);
+    System.out.println(A >= C);
+    System.out.println(C >= B);
+    
+    // < Küçüktür operatörü
+    System.out.println(A < D);
+    System.out.println(D < C);
+    System.out.println(C < B);
+    
+    // <= Küçük-Eşittir operatörü
+    System.out.println(A <= D);
+    System.out.println(A <= C);
+    System.out.println(C <= B);
+      
+        
+    }
+    
+}
