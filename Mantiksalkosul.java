aef5726c6401b5b47c0893ff5fdd7a5a57bb1c57
GamzeNurAslan <150529563+GamzeNurAslan@users.noreply.github.com>
2023-11-23 10:49:02 +0300

    Add files via upload
    Mantıksal koşul örneğini switch-case yapısını kullanarak yazma

---
+++ Mantiksalkosul.java
@@ -0,0 +1,43 @@
+package mantiksalkosul;
+
+public class Mantiksalkosul {
+public static void main(String[] args) {
+    int day=7;
+    switch (day){
+        case 1:
+            System.out.print("Hafta içi günlerinden ");
+                System.out.println("Pazartesi günündesiniz");
+              break;
+        case 2:
+            System.out.print("Hafta içi günlerinden ");
+                System.out.println("Salı günündesiniz");
+              break;
+        case 3:
+            System.out.print("Hafta içi günlerinden ");
+                System.out.println("Çarşamba günündesiniz");
+              break;
+        case 4:
+            System.out.print("Hafta içi günlerinden ");
+                System.out.println("Perşembe günündesiniz");
+              break;
+        case 5:
+            System.out.print("Hafta içi günlerinden ");
+                System.out.println("Cuma günündesiniz");
+              break;
+              
+        case 6:
+            System.out.print("Hafta sonu günlerinden ");
+                System.out.println("Cumartesi günündesiniz");
+              break;
+        case 7:
+            System.out.print("Hafta sonu günlerinden ");
+                System.out.println("Pazar günündesiniz");
+              break;
+        
+    }
+    
+       
+    }
+
+    
+}
