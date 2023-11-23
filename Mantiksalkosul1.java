0778a9f7e1a7e0552258ce84d5c01755dc5f8282
GamzeNurAslan <150529563+GamzeNurAslan@users.noreply.github.com>
2023-11-23 10:58:59 +0300

    Add files via upload
    Mantıksal koşul örneğini if-else yapısını kullanarak yazma

---
+++ Mantiksalkosul1.java
@@ -0,0 +1,28 @@
+package mantiksalkosul1;
+
+public class Mantiksalkosul1 {
+public static void main(String[] args) {
+     int day = 7;
+    if (day == 1){
+        System.out.println("Pazartesi günündesiniz.");
+    }else if (day == 2){
+        System.out.println("Salı günündesiniz.");
+    }else if (day == 3){
+        System.out.println("Çarşamba günündesiniz.");
+    }else if (day == 4){
+        System.out.println("Perşembe günündesiniz.");
+    }else if (day == 5){
+        System.out.println("Cuma günündesiniz.");
+    }else if (day == 6){
+        System.out.println("Cumartesi günündesiniz.");
+    }else{
+        System.out.println("Pazar günündesiniz.");
+        
+        
+    }
+        
+    }
+      
+    }
+    
+
