7ce2cb10dbbe4bd70144633aecabd6918a66bc05
GamzeNurAslan <150529563+GamzeNurAslan@users.noreply.github.com>
2023-12-07 23:00:08 +0300

    For döngüsüyle üslü sayı hesaplama

--- Uslusayi.java
+++ Uslusayi.java
@@ -1,15 +1,26 @@
 package uslusayi;
-import java.util.Scanner;
-public class Uslusayi {
-public static void main(String[] args) {
-         Scanner klavye=new Scanner(System.in);
+/*Scanner klavye=new Scanner(System.in);
        int n,r,uslu;
        System.out.println("Üssü alınacak sayıyı giriniz:");
        n=klavye.nextInt();
        System.out.println("Üs olacak sayıyı giriniz:");
        r=klavye.nextInt();
        uslu=(int) Math.pow(n,r);
-       System.out.println("Üslü ifade="+uslu);
+       System.out.println("Üslü ifade="+uslu);*/
+//*****************************************************************************//
+import java.util.Scanner;
+public class Uslusayi {
+public static void main(String[] args) {
+         Scanner klavye=new Scanner(System.in);
+         int n,r,toplam=1;
+         System.out.println("Üssü alınacak sayıyı giriniz:");
+         n=klavye.nextInt();
+         System.out.println("Üs olacak sayıyı giriniz:");
+         r=klavye.nextInt();
+         for(int i=1;i<=r;i++){
+             toplam*=n;
+             System.out.println(""+toplam);
+         }
     }
     
 }
