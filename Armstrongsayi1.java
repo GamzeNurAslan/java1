b22c457e991087fae370d2ec1fe77b7ad878522a
GamzeNurAslan <150529563+GamzeNurAslan@users.noreply.github.com>
2023-12-08 18:06:01 +0300

    Yazılan sayının rakamlarının toplamı:))

--- Armstrongsayi1.java
+++ Armstrongsayi1.java
@@ -2,35 +2,17 @@ package armstrongsayi1;
 import java.util.Scanner;
 public class Armstrongsayi1 {
 public static void main(String[] args) {
-     Scanner klavye=new Scanner(System.in);
-        int sayi,b;
-        System.out.println("Bir sayı giriniz:");
-        sayi=klavye.nextInt();
-        int basamaknumarasi=0;
-        int tempNumber=sayi;
-        int basValue;
-        int result=0;
-        int basPow;
-        while(tempNumber != 0){
-        tempNumber/=10;
-        basamaknumarasi++;
-        }
-        tempNumber=sayi;
-        while(tempNumber != 0){
-        basValue=tempNumber%10;
-        basPow=1;
-        for(int i=1;i<=basamaknumarasi;i++){
-        basPow*=basValue;
-        }
-        result+=basPow;
-        tempNumber/=10;
-        }
-        if(result== sayi){
-            System.out.println(sayi + " sayısı bir Armstrong sayıdır.");
-        }else{
-            System.out.println(sayi + " sayısı Armstrong sayı değildir.");
-        }
-        
+    Scanner klavye=new Scanner(System.in);
+    System.out.print("Bir sayı giriniz:");
+    int sayi= klavye.nextInt();
+    int basamak=0;
+    int toplam=0;
+    while(sayi!=0){
+    toplam+=(sayi%10);
+    sayi/=10;
+    ++basamak;
+    }
+    System.out.println("Basamak toplamı="+toplam);
     }
     
 }
