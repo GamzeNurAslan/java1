b802368af985f6add75f1d42ecc8f5a325f8a8bd
GamzeNurAslan <150529563+GamzeNurAslan@users.noreply.github.com>
2023-12-14 10:54:07 +0300

    Update Mukemmelsayi.java

--- Mukemmelsayi.java
+++ Mukemmelsayi.java
@@ -1,4 +1,5 @@
 package mukemmelsayi;
+/*Mükemmel sayı=Bir sayının kendisi hariç pozitif tam sayı çarpanlarının toplamı kendisine eşit olan sayıya Mükemmel sayı denir.*/
 import java.util.Scanner;
 public class Mukemmelsayi {
 public static void main(String[] args) {
