d766de22794901b5714865e09d17eae0a267ac74
GamzeNurAslan <150529563+GamzeNurAslan@users.noreply.github.com>
2023-12-14 11:52:48 +0300

    Fibonaccidizisi.java

--- Fibonaccidizisi.java
+++ Fibonaccidizisi.java
@@ -1,4 +1,9 @@
 package fibonaccidizisi;
+/*Fibonacci serisi, her sayının kendinden önceki ile toplanması sonucu oluşan bir sayı dizisidir.
+ Bu şekilde devam eden bu dizide sayılar birbirleriyle oranlandığında altın oran ortaya çıkar,
+ yani bir sayı kendisinden önceki sayıya bölündüğünde altın orana gittikçe yaklaşan bir dizi elde edilir.
+Fibonacci dizisi, 0'dan başlar ve sonsuza kadar. Her rakam, bir önceki rakamla toplanır.*/
+
 import java.util.Scanner;
 public class Fibonaccidizisi {
 public static void main(String[] args) {
