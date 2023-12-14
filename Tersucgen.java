15227f79e4778e53f514ae9117ea48b7e30d833e
GamzeNurAslan <150529563+GamzeNurAslan@users.noreply.github.com>
2023-12-14 11:09:29 +0300

    Yıldızlarla ters üçgen yapma

---
+++ Tersucgen.java
@@ -0,0 +1,19 @@
+package tersucgen;
+import java.util.Scanner;
+public class Tersucgen {
+public static void main(String[] args) {
+        Scanner klavye=new Scanner(System.in);
+        System.out.print("Basamak sayısını giriniz=");
+        int n=klavye.nextInt();
+        for(int i=n;i>=1;i--){
+          for(int j=1;j<=(n-1);j++){
+              System.out.print(" ");
+          }
+          for(int k=1;k<=(2*i+1);k++){
+              System.out.print("*");
+          }
+            System.out.println();
+        }
+    }
+    
+}
