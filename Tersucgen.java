d4d9b40a4041d6942e6ff7ff6efcb0e54c99fc32
GamzeNurAslan <150529563+GamzeNurAslan@users.noreply.github.com>
2023-12-14 11:16:23 +0300

    Tersucgen.java

--- Tersucgen.java
+++ Tersucgen.java
@@ -7,7 +7,7 @@ public static void main(String[] args) {
         int n=klavye.nextInt();
         for(int i=n;i>=1;i--){
           for(int j=1;j<=(n-1);j++){
-              System.out.print(" ");
+              System.out.print("");
           }
           for(int k=1;k<=(2*i+1);k++){
               System.out.print("*");
