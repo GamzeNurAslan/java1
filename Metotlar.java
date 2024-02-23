8a0494218a97754080fe0a2e96dd0330af982e7d
GamzeNurAslan <150529563+GamzeNurAslan@users.noreply.github.com>
2024-02-23 15:29:57 +0300

    Metotlarla toplama

---
+++ Metotlar.java
@@ -0,0 +1,58 @@
+package metotlar;
+/*package metotlar;
+public class Metotlar {
+    static void helloWorld(){
+        System.out.println("Merhaba Türkiye");
+    }
+    static int power (int base,int exp){
+       int result = 1;
+       for(int i=1;i<=exp;i++){
+           result*=base;
+       }
+       return result;
+    }
+public static void main(String[] args) {
+        //f(x)= 2x+5
+                //f(x)=2*6+5=17
+                System.out.println(power(2,3));
+                System.out.println(power(4,2));
+               helloWorld();
+               helloWorld();
+               helloWorld();
+               helloWorld();
+               helloWorld();
+               
+    }
+
+    
+    }
+*/
+//*****************************************************************************//
+public class Metotlar {
+    static void helloWorld(){
+        System.out.println("Merhaba Türkiye");
+    }
+    static int power (int base,int exp){
+       int result = 1;
+       for(int i=1;i<=exp;i++){
+           result*=base;
+       }
+       return result;
+    }
+public static void main(String[] args) {
+        //f(x)= 2x+5
+                //f(x)=2*6+5=17
+                System.out.println(power(2,3));
+                System.out.println(power(4,2));
+               helloWorld();
+               helloWorld();
+               helloWorld();
+               helloWorld();
+               helloWorld();
+               
+    }
+
+    
+    }
+    
+
